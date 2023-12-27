a118f5c708468fcf84b8dccd8155312ba2a6ddf4
Ritik Giri <giri.ritik.2002@gmail.com>
2023-12-27 18:23:12 +0530

    changed in bg in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 function Footer() {
   return (
-    <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4 !mt-32 bg-gray-200">
+    <footer className="bg-gray-200 rounded-lg shadow dark:bg-gray-900 m-4 !mt-32">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <Link
