304271a1c0cf99383768bfb30c5e5d13e6561a0f
Ritik Giri <giri.ritik.2002@gmail.com>
2023-12-27 18:19:22 +0530

    bg added in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 function Footer() {
   return (
-    <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4 !mt-32">
+    <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4 !mt-32 bg-gray-200">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <Link
@@ -12,7 +12,13 @@ function Footer() {
             className="flex p-0 items-center sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
             <div className="self-center m-auto w-[50%] h-[50%] font-bold whitespace-nowrap text-[#ce9251] dark:text-white dark:hover:text-[#ce9251]">
-              <Image className="" src="/logo.png" width={500} height={500} alt="logo" />
+              <Image
+                className=""
+                src="/logo.png"
+                width={500}
+                height={500}
+                alt="logo"
+              />
             </div>
           </Link>
           <ul className="flex flex-wrap justify-center items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400 ">
@@ -53,10 +59,7 @@ function Footer() {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block justify-center text-sm text-gray-500 text-center dark:text-gray-400">
           © 2023{" "}
-          <Link
-            href="/"
-            className="hover:underline hover:text-[#ce9251]"
-          >
+          <Link href="/" className="hover:underline hover:text-[#ce9251]">
             DevTechsposure™
           </Link>
           . All Rights Reserved.
